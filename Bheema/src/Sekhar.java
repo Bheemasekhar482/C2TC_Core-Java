0d5cd5e9f999d271c4d37093546d05bef0eb4117
Admin <Admin@DESKTOP-KCJO4M4>
2022-05-23 13:51:47 +0530

    first commit

--- Bheema/src/Sekhar.java
+++ Bheema/src/Sekhar.java
@@ -3,7 +3,10 @@ public class Sekhar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    int a=10;
+    int b=20;
+    int c=a-b;
+    System.out.println(c);
 	}
 
 }
---
+++ Bheema/src/pyatlo.java
@@ -0,0 +1,12 @@
+
+public class pyatlo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double i=10.0;
+		double j=30.0;
+		double c=i*j;
+      System.out.println(c);
+	}
+
+}
