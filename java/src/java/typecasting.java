c3f5f8b927308eeefddff91f620a0a06b9347358
Admin <Admin@DESKTOP-KCJO4M4>
2022-05-23 13:26:39 +0530

    first commit

---
+++ java/src/java/typecasting.java
@@ -0,0 +1,13 @@
+package java;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=9;
+		double j=i;
+		System.out.println(i);
+		System.out.println(j);
+	}
+
+}
