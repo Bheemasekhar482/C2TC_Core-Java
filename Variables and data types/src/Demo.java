3efa96a536c5599b92d21d8d36c9c1974c6caad7
Admin <Admin@DESKTOP-KCJO4M4>
2022-05-24 19:39:14 +0530

    first commit

---
+++ Variables and data types/src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+	static void Demo(float x) {
+		System.out.println("float");
+	}
+	static void Demo(double x) {
+		System.out.println("double");
+	}
+	public static void main(String[] bheema) {
+		Demo(99.9);
+	}
+}
